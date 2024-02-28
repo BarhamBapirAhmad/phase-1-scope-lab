2bf8f33151232cc38ee6d76214e8ecb0d0c4f866
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-02-28 07:15:19 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+function fixTHescope() {
+    const two = 2;
+    const three = 3;
+    return function() {
+      return two + three;
+    };
+  }
+  var customerName = 'bob';
+  function customerName() {
+      return customerName;
+    }
+    function upperCaseCustomerName() {
+        customerName=  customerName.toUpperCase();
+    return customerName;
+    }
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+    }
+    function overwriteBestCustomer() {
+        bestCustomer = 'maybe bob'; 
+    }
+    const leastFavoriteCustomer = ' ';
+    function changeLeastFavoriteCustomer() {
+        leastFavoriteCustomer = ' ';
+    } 
+
+
+
+
